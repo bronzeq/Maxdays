420ad0225d68e536691101e9f2438421c6c7851d
bronzeq <36151742+bronzeq@users.noreply.github.com>
2020-01-29 17:35:58 +0900

    Update MaxDay.java

--- MaxDay/src/MaxDay.java
+++ MaxDay/src/MaxDay.java
@@ -12,13 +12,9 @@ public class MaxDay {
 	{
 		 System.out.printf("%4d %3d/n",year month);
 		System.out.println(
-				"일 월 화 수 목 금 토\r\n" + 
-				"--------------------\r\n" + 
-				" 1  2  3  4  5  6  7\r\n" + 
-				" 8  9 10 11 12 13 14\r\n" + 
-				"15 16 17 18 19 20 21\r\n" + 
-				"22 23 24 25 26 27 28");		
+				 System.out.printf("%4d %3d/n",year, month);
+		System.out.println(	"SU MO TU WE  TH FR SA"); 
+		System.out.println("----------------------------------" );	
 	}
 
-//public static char[7][5] Calinder (int month ) {}
 
