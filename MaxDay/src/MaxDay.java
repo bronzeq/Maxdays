6ddb08a546a426703c5e6574c404e1e3a2c007c1
bronzeq <36151742+bronzeq@users.noreply.github.com>
2020-01-29 17:38:01 +0900

    Update MaxDay.java

--- MaxDay/src/MaxDay.java
+++ MaxDay/src/MaxDay.java
@@ -3,18 +3,26 @@ import java.util.Scanner;
 public class MaxDay {
 	
 	private static  final int[] MAX_DAYS= {31,28,31,30,31,30,31,31,30,31,30,31};
-	public  int GetMonthOfMaxDays(Scanner scanner){
-		return MAX_DAYS[scanner-1];
+	private static  final int[] LEEP_MAX_DAYS= {31,29,31,30,31,30,31,31,30,31,30,31};
+	public boolean isLeepYear(int year) {
+		if(year%4==0&&(year%100 !=0||year%400==0))
+			return true;
+		else return false;
+	}
+		
+	public  int GetMaxDays(int year,int month){
+		return MAX_DAYS[month-1];
 		}
 	}
 
 	 public void samplecalendar(int year, int month)
 	{
-		 System.out.printf("%4d %3d/n",year month);
-		System.out.println(
-				 System.out.printf("%4d %3d/n",year, month);
+		 System.out.printf("%4d %3d/n",year, month);
 		System.out.println(	"SU MO TU WE  TH FR SA"); 
-		System.out.println("----------------------------------" );	
+		System.out.println("----------------------------------" );
+	int maxday=GetMaxDays( year, month)
+		
+				
 	}
 
-
+}
